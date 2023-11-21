36c6e52510f75012f041ef84ab25ea0ea5985731
deonjoshua <deonjoshua1997@gmail.com>
2023-11-21 16:08:11 +1100

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
-Tool.destroy_all
-# puts "seeding database"
-# 20.times do
-#   Tool.create!(
-#     name: Faker::Name.name,
-#     description: Faker::Lorem.sentence,
-#     rate: Faker::Number.number(digits: 2),
-#     condition:"Excellent",
-#     image_url:Faker::Placeholdit.image(size: '150x150', format: 'jpeg', background_color: :random),
-#     user: User.find(1)
-#   )
-# end
-# puts "finished"
 require "open-uri"
-
-file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
-article = Tool.new(
-  name: Faker::Name.name,
-  description: Faker::Lorem.sentence,
-  rate: Faker::Number.number(digits: 2),
-  condition:"Excellent",
-  user: User.first
-)
-article.image.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save!
+Tool.destroy_all
+puts "seeding database"
+20.times do
+  file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+  article = Tool.new(
+    name: Faker::Name.name,
+    description: Faker::Lorem.sentence,
+    rate: Faker::Number.number(digits: 2),
+    condition:"Excellent",
+    user: User.first
+  )
+  article.image.attach(io: file, filename: "nes.png", content_type: "image/png")
+  article.save!
+end
+puts "finished"
