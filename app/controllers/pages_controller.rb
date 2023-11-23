e8a863ff666e3331751c561f63627164bbf804b3
deonjoshua <deonjoshua1997@gmail.com>
2023-11-23 16:09:29 +1100

    fix calendar bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,7 +21,6 @@ class PagesController < ApplicationController
 
   def show
     @user = current_user
-    @tools = Tool.where(user_id:@user_id)
-    raise
+    @tools = Tool.where(user_id:@user.id)
   end
 end
