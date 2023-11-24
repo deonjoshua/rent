231ce520aa08a2e072033edf3eb81c53243b6ead
deonjoshua <deonjoshua1997@gmail.com>
2023-11-24 12:20:36 +1100

    Added rating to Tools and Added image_url to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_013016) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_24_011806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_013016) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["user_id"], name: "index_tools_on_user_id"
   end
 
@@ -80,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_013016) do
     t.string "phone_number"
     t.float "latitude"
     t.float "longitude"
+    t.string "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
